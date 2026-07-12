ff3c78a746c12469c9fea8bf3337f414a0e53f76
allison179 <suvarnaallison17@gmail.com>
2026-07-12 09:40:27 +0000

    Final

--- src/testapi.py
+++ src/testapi.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="YOUR_AQ_KEY")
+client = genai.Client(api_key="AQ.Ab8RN6KF0oA2EhwbvrI5BxymmiqrRrjmfc53wu9lPJ_He37YGg")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
